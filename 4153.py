fc530e7b930094a2f2e7e8cd7adc2e27c28d3d1b
Moona <moonapark1126@gmail.com>
2023-05-18 09:21:34 +0900

    4153

--- 4153.py
+++ 4153.py
@@ -34,4 +34,16 @@ for i in range(len(A)):
         B.append('wrong')
 for i in B:
     print(i)    
-# 
+# 보기의 예시를 다 따라할 수 있다, 그리고 3가지 요소의 크기가 큰 경우를 골라서 정렬하는 것도 구현 함..
+while True :
+    nums = list(map(int, input().split()))
+    if sum(nums) == 0:
+        break  # 세 수가 0이면 break
+    max_num = max(nums)
+    nums.remove(max_num)  # 빗변의 길이는 직각삼각형 세변의 길이중 가장 길다.
+    if nums[0]**2 + nums[1]**2 == max_num**2:
+        print('right')
+    else:
+        print('wrong')
+# 비교 안하고 맥스 써도 좋구나.. 
+#
