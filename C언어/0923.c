f301fe67b86932346e34930df48be511ab7ccc12
moona <moonapark1126@gmail.com>
2024-09-23 19:10:25 +0900

    0923 end

--- C언어/0923.c
+++ C언어/0923.c
@@ -250,29 +250,274 @@
 //    }
 //    printf("%d",max-min);
 //}
-#include <stdio.h>
-#include <malloc.h>
-int main()
-{
-    int a, i;
-    scanf("%d", &a);
-    int arr[a];
-    int* p =(int*)malloc(sizeof(int)*a);
-    int min=1000, max=0;
+//#include <stdio.h>
+//#include <malloc.h>
+//int main()
+//{
+//    int a, i;
+//    scanf("%d", &a);
+//    int arr[a];
+//    int* p =(int*)malloc(sizeof(int)*a);
+//    int min=1000, max=0;
+//
+//    for(i=0; i<a; i++){
+//        scanf("%d", p+i);
+//    }
+//
+//    for(i=0;i<a;i++){
+//        if(*(p+i)>max){
+//            max=*(p+i);
+//        }
+//        if(*(p+i)<min){
+//            min=*(p+i);
+//        }
+//    }
+//    printf("%d",max-min);
+//    free(p);
+//}
+//1410
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//    char arr[1000001];
+//    int i, len;
+//    int open=0, close=0;
+//    scanf("%s",arr);
+//    len =strlen(arr);
+//    for(i=0;i<len;i++){
+//        if(arr[i] == '('){
+//            open++;
+//           }else{
+//            close++;
+//           }
+//    }
+//    printf("%d %d", open, close);
+//}
+//5079
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//    int a, i;
+//    int _a=0, _b=0;
+//    scanf("%d", &a);
+//    char arr[a+1];
+//    scanf("%s",arr);
+//    for(i=0;i<a;i++){
+//        if(arr[i] == 'A'){
+//            _a++;
+//        }else if(arr[i] == 'B'){
+//            _b++;
+//        }
+//    }
+//    if(_a>_b){
+//        printf("A");
+//    }else if(_a<_b){
+//        printf("B");
+//    }else{
+//        printf("Tie");
+//    }
+//
+//}
+//1408
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//    char arr[21];
+//    int i, len;
+//    scanf("%s",arr);
+//    len = strlen(arr);
+//    for(i=0; i<len; i++){
+//        printf("%c", arr[i]+2);
+//    }
+//    printf("\n");
+//
+//    for(i=0; i<len; i++){
+//        printf("%c", (arr[i]*7)%80+48);
+//    }
+//}
+//1295
+//#include <stdio.h>
+//#include <string.h>
+//#include <ctype.h>
+//int main()
+//{
+//    char arr[1001];
+//    int i, len;
+//    scanf("%s", arr);
+//    len = strlen(arr);
+//    for(i=0;i<len;i++){
+//        if(islower(arr[i])){
+//            printf("%c", toupper(arr[i]));
+//        }else if(isupper(arr[i])){
+//                 printf("%c",tolower(arr[i]));
+//        }else{
+//            printf("%c",arr[i]);
+//        }
+//    }
+//}
+//4771
+////1500
+//#include <stdio.h>
+//#include <string.h>
+//#include <ctype.h>
+//int main()
+//{
+//    int a, b, i, j;
+//    scanf("%d %d", &a, &b);
+//    int arr[a][b];
+//
+//    for(i=0; i<a; i++){
+//        for(j=0; j<b; j++){
+//            scanf("%d", &arr[i][j]);
+//        }
+//    }
+//    for(i=0; i<a; i++){
+//        for(j=0; j<b; j++){
+//            printf("%d ", arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
 
-    for(i=0; i<a; i++){
-        scanf("%d", p+i);
-    }
+//1096
+//#include <stdio.h>
+//#include <string.h>
+//#include <ctype.h>
+//int main()
+//{
+//    int arr[19][19] = {0};
+//    int i, j;
+//    int a,b,c;
+//
+//    scanf("%d", &a);
+//    for(i=0;i<a;i++){
+//        scanf("%d %d", &, &c);
+//        arr[b-1][c-1] = 1;
+//    }
+//    for(i=0; i<19; i++){
+//        for(j=0; j<19; j++){
+//            printf("%d ", arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
+////1402
+//#include <stdio.h>
+//#include <string.h>
+//#include <ctype.h>
+//int main()
+//{
+//    int a, i;
+//    scanf("%d", &a);
+//    int arr[a];
+//    for(i=0; i<a;i++){
+//        scanf("%d", &arr[i]);
+//    }
+//    for(i=a-1;i>=0; i--){
+//        printf("%d ", arr[i]);
+//    }
+//}
+//1660
+//#include <stdio.h>
+//#include <string.h>
+//#include <ctype.h>
+//int main()
+//{
+//   char arr[101];
+//   int i, len;
+//   scanf("%s", arr);
+//   len = strlen(arr);
+//   for(i=0;i<len; i++){
+//    if(arr[i] == ','){
+//        printf(" ");
+//    }else{
+//        printf("%c", arr[i]);
+//    }
+////   }
+////}
+////동적 할당
+//#include <stdio.h>
+//#include <string.h>
+//#include <ctype.h>
+//#include <malloc.h>
+//int main()
+//{
+//   char *p = (char *)malloc(sizeof(char)*101);
+//   int i, len;
+//   scanf("%s", p);
+//   len = strlen(p);
+//   for(i=0;i<len; i++){
+//    if(*(p+i) == ','){
+//        printf(" ");
+//    }else{
+//        printf("%c", *(p+i));
+//    }
+//   }
+//   free(p);
+//}
+//1674
+//#include <stdio.h>
+//#include <string.h>
+//#include <ctype.h>
+//#include <malloc.h>
+//int main()
+//{
+//    int sum=0, i;
+//    for(i=1; i<=10; i++){
+//        scanf("%1d", &a);
+//        sum = sum + a;
+//    }
+//    if(sum%7==4){
+//        printf("Bad");
+//    }else{
+//        printf("Good");
+//    }
+//}
+//1707
+//#include <stdio.h>
+//#include <string.h>
 
-    for(i=0;i<a;i++){
-        if(*(p+i)>max){
-            max=*(p+i);
-        }
-        if(*(p+i)<min){
-            min=*(p+i);
-        }
-    }
-    printf("%d",max-min);
-    free(p);
-}
-
+//int main()
+//{
+//    int arr[10];
+//    int sum=0;
+//    int i, big=0, small = 0;
+//    double avg;
+//
+//    for(i=0;i<10; i++){
+//        scanf("%d", &arr[i]);
+//        sum = sum + arr[i];
+//    }
+//    avg = (double)sum/10;
+//    printf("%.1lf\n", avg);
+//
+//    for(i=0; i<10; i++){
+//        if(arr[i] >= avg){
+//            big++;
+//        }else{
+//            small++;
+//        }
+//    }
+//    printf("%d %d", big, small);
+//}
+//2081
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//    int max=0;
+//    int pos, a, i;
+//    for(i=1; i<=9; i++){
+//        scanf("%d", &a);
+//        if(a>max){
+//            max = a;
+//            pos = i;
+//        }
+//    }
+//    printf("%d\n",max);
+//    printf("%d",pos);
+//}
